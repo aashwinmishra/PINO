910c3b30e87e705826067e64421d9bd680eec59c
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2026-01-28 18:45:41 -0800

    Update models.py

--- models.py
+++ models.py
@@ -59,15 +59,59 @@ class SpectralConv2d(nn.Module):
     self.modes1 = modes1
     self.modes2 = modes2
     self.out_channels = out_channels
-    self.scale = 1.0 / (in_channels * out_channels)
-    self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, modes1, modes2, dtype=torch.cfloat))
-    self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, modes1, modes2, dtype=torch.cfloat))
+    self.scale = (2.0 / (in_channels + out_channels))**0.5
+    self.weights1 = nn.Parameter(self.scale * torch.randn(in_channels, out_channels, modes1, modes2, dtype=torch.cfloat))
+    self.weights2 = nn.Parameter(self.scale * torch.randn(in_channels, out_channels, modes1, modes2, dtype=torch.cfloat))
 
   def forward(self, x: torch.tensor):
     x_ft = torch.fft.rfft2(x)                               #[B, C, H, W//2 + 1]
     out_ft = torch.zeros(x.shape[0], self.out_channels, x_ft.shape[-2], x_ft.shape[-1], device=x.device, dtype=torch.cfloat)
-    out_ft[:, :, :self.modes1, :self.modes2] = (x_ft[:,:,:self.modes1, :self.modes2].permute(2, 3, 0, 1) @ self.weights1.permute(2, 3, 0, 1)).permute(2, 3, 0, 1)
-    out_ft[:, :, -self.modes1:, :self.modes2] = (x_ft[:,:,-self.modes1:, :self.modes2].permute(2, 3, 0, 1) @ self.weights1.permute(2, 3, 0, 1)).permute(2, 3, 0, 1)
+    # out_ft[:, :, :self.modes1, :self.modes2] = (x_ft[:,:,:self.modes1, :self.modes2].permute(2, 3, 0, 1) @ self.weights1.permute(2, 3, 0, 1)).permute(2, 3, 0, 1)
+    # out_ft[:, :, -self.modes1:, :self.modes2] = (x_ft[:,:,-self.modes1:, :self.modes2].permute(2, 3, 0, 1) @ self.weights1.permute(2, 3, 0, 1)).permute(2, 3, 0, 1)
+    # Multiply top-left corner (positive frequencies)
+    out_ft[:, :, :self.modes1, :self.modes2] = torch.einsum("bixy,ioxy->boxy", x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+    # Multiply bottom-left corner (negative frequencies)
+    out_ft[:, :, -self.modes1:, :self.modes2] = torch.einsum("bixy,ioxy->boxy",x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
     return torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
+
+
+class FNO2d(nn.Module):
+  def __init__(self, 
+               modes1: int, 
+               modes2: int, 
+               width: int, 
+               num_layers: int,
+               padding_frac: float=1.0,
+               in_channels: int=1):
+    super().__init__()
+    self.modes1 = modes1 
+    self.modes2 = modes2
+    self.width = width 
+    self.num_layers = num_layers 
+    self.padding_frac = padding_frac
+    self.linear_p = nn.Linear(in_channels, width)
+    self.conv_list = nn.ModuleList([
+        nn.Conv2d(width, width, 1) for _ in range(num_layers)
+    ])
+    self.spec_list = nn.ModuleList([
+        SpectralConv2d(width, width, modes1, modes2) for _ in range(num_layers)
+    ])
+    self.linear_q = nn.Linear(width, 128)
+    self.output_layer = nn.Linear(128, 1)
+    self.activation = nn.GELU()
+
+  def forward(self, x: torch.tensor):
+    x = self.linear_p(x)                                    #[B, N, N, C] -> [B, N, N, W]
+    x = x.permute(0, 3, 1, 2)                               #[B, W, N, N]
+    x1_padding = int(round(x.shape[-1] * self.padding_frac))
+    x2_padding = int(round(x.shape[-2] * self.padding_frac))
+    x = nn.functional.pad(x, [0, x1_padding, 0, x2_padding])
+    for s, c in zip(self.spec_list, self.conv_list):
+      x = self.activation(s(x) + c(x))
+     x = x[..., :x.size(-2)-x2_padding, :x.size(-1)-x1_padding]
+    x = x.permute(0, 2, 3, 1)
+    x = self.linear_q(x)
+    x = self.activation(x)
+    return self.output_layer(x)  
 
 
